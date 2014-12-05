b424224ffa66d2447a9dd7bcd543896e1f288293
Lucas Costa <lucas@lucascosta.com>
2014-12-05 01:54:12 -0200

    Gulp
    JSHint and Mocha

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+/* jshint node:true */
+'use strict';
+
+var gulp = require('gulp');
+var $ = require('gulp-load-plugins')();
+
+gulp.task('jshint', function () {
+  return gulp.src('src/**/*.js')
+    .pipe($.jshint())
+    .pipe($.jshint.reporter('jshint-stylish'))
+    .pipe($.jshint.reporter('fail'));
+});
+
+gulp.task('test', function () {
+  return gulp.src('test/**/*.js', {read: false})
+    .pipe($.mocha({reporter: 'nyan'}));
+});
+
+gulp.task('watch', function () {
+  gulp.watch('src/**/*.js', ['jshint','test']);
+  gulp.watch('test/**/*.js', ['test']);
+});
+
+gulp.task('default', function () {
+  gulp.start('watch');
+});

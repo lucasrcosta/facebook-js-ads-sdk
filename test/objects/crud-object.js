567ed34ac98db2bd68a128d55455952ebd09be69
Lucas Costa <lucas@lucascosta.com>
2015-01-07 16:55:20 -0200

    unique api for each test

--- test/objects/crud-object.js
+++ test/objects/crud-object.js
@@ -7,8 +7,8 @@ if (typeof require === 'function') {
 describe('CrudObject', function() {
   'use strict';
 
+  var token = 'a1b2c3d4e5';
   var CrudObject = FacebookAdsApi.objects.CrudObject;
-  var api = new FacebookAdsApi('a1b2c3d4e5');
 
   describe('constructor', function() {
 
@@ -21,11 +21,13 @@ describe('CrudObject', function() {
   describe('object properties', function() {
 
     it('contains an API instance', function() {
+      var api = new FacebookAdsApi(token);
       var crudObj = new CrudObject(api, 'endpoint', []);
       crudObj.getApi().should.be.eql(api);
     });
 
     it('stores an endpoint', function() {
+      var api = new FacebookAdsApi(token);
       var crudObj = new CrudObject(api, 'endpoint', []);
       crudObj.getEndpoint().should.be.eql('endpoint');
     });

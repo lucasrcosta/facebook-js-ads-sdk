40ecc9e50d0efc726d014cee394ddc2d599db63e
Lucas Costa <lucas@lucascosta.com>
2015-02-23 17:54:51 -0300

    adGroup live test (img failing)

--- live/objects/ad-group.js
+++ live/objects/ad-group.js
@@ -3,41 +3,59 @@ describe('AdGroup', function() {
 
   var adCampaign;
   var setId;
+  var adCreative;
   var groupId;
   var now = (new Date()).toUTCString();
-  var groupData = {};
+  var groupData = {
+    name: 'SDK TEST AD-GROUP - ' + now,
+  };
 
-  // before(function(done) {
-  //   adCampaign = new api.AdCampaign({
-  //     name: 'SDK TEST AD-GROUP CAMPAIGN - ' + now
-  //   }, testData.accountId);
-  //   adCampaign.create().then(function() {
-  //     var setData = {
-  //       bid_info:{'IMPRESSIONS':50},
-  //       bid_type:'ABSOLUTE_OCPM',
-  //       campaign_status:'PAUSED',
-  //       campaign_group_id: adCampaign.id,
-  //       daily_budget:100,
-  //       name:'SDK TEST AD-SET',
-  //       start_time:1424363064,
-  //       targeting:{'geo_locations':{'countries':['BR']}},
-  //     };
-  //     var adSet = new api.AdSet(setData, testData.accountId);
-  //     adSet.create().then(function() {
-  //       groupData.campaign_id = setId;
-  //       done();
-  //     });
-  //   })
-  //   .catch(done);
-  // });
-  groupData.campaign_id = 6019705918994;
-  // after(function(done) {
-  //   adCampaign.delete()
-  //     .then(function(data) {
-  //       done();
-  //     })
-  //     .catch(done);
-  // });
+  before(function(done) {
+    adCampaign = new api.AdCampaign({
+      name: 'SDK TEST AD-GROUP CAMPAIGN - ' + now
+    }, testData.accountId);
+    adCampaign.create().then(function() {
+      var setData = {
+        bid_info:{'IMPRESSIONS':50},
+        bid_type:'ABSOLUTE_OCPM',
+        campaign_status:'PAUSED',
+        campaign_group_id: adCampaign.id,
+        daily_budget:100,
+        name:'SDK TEST AD-SET',
+        start_time:1424363064,
+        targeting:{'geo_locations':{'countries':['BR']}},
+      };
+      var adSet = new api.AdSet(setData, testData.accountId);
+      adSet.create().then(function() {
+        groupData.campaign_id = adSet.id;
+        var creativeData = {
+          name: 'SDK TEST AD-CREATIVE - ' + now,
+          title: 'Title for Ad Creative',
+          body: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit.',
+          object_url: 'http://www.facebook.com',
+          image_hash: 'dbce3bf6b57da4ec23359019cb14f8af'
+        };
+        adCreative = new api.AdCreative(creativeData, testData.accountId);
+        adCreative.create().then(function() {
+          groupData.creative = {'creative_id': adCreative.id};
+          done();
+        });
+      });
+    })
+    .catch(done);
+  });
+
+  after(function(done) {
+    adCampaign.delete()
+      .then(function(data) {
+        adCreative.delete()
+          .then(function(data) {
+            data.success.should.be.true;
+            done();
+          });
+      })
+      .catch(done);
+  });
 
   it('creates', function(done) {
     var adGroup = new api.AdGroup(groupData, testData.accountId);

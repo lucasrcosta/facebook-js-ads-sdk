1f571412557ed9acd3e3bf060219fded103326a9
Lucas Costa <lucas@lucascosta.com>
2015-02-26 16:22:17 -0300

    Accretive live tests using AdImage

--- live/objects/ad-creative.js
+++ live/objects/ad-creative.js
@@ -1,8 +1,29 @@
 describe('AdCreative', function() {
   'use strict';
 
-  var  creativeId;
+  var creativeId;
+  var adImage;
   var now = (new Date()).toUTCString();
+
+  before(function(done) {
+    if (!FormData)
+      throw new Error('FormData missing');
+    var formData = new FormData();
+    formData.append('1200x628.gif', testImages.gif_1280_628, '1200x628.gif');
+    adImage = new api.AdImage(null, testData.accountId);
+    adImage.create(formData).then(function() {
+      done();
+    })
+    .catch(done);
+  });
+
+  after(function(done) {
+    adImage.delete()
+      .then(function(data) {
+        done();
+      })
+      .catch(done);
+  });
 
   it('creates', function(done) {
     var creativeData = {
@@ -10,7 +31,7 @@ describe('AdCreative', function() {
       title: 'Title for Ad Creative',
       body: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit.',
       object_url: 'http://www.facebook.com',
-      image_hash: 'dbce3bf6b57da4ec23359019cb14f8af' // flag
+      image_hash: adImage.hash
     };
     var adCreative = new api.AdCreative(creativeData, testData.accountId);
     adCreative.create().then(function() {

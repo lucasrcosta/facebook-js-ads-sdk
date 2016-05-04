8123496ba53e33998edcab8a72ab5551279a2fc0
Lucas Costa <lucas@lucascosta.com>
2016-05-04 00:46:17 -0300

    Abstract Crud Object data operations

---
+++ src/objects/core/abstract_crud_object.js
@@ -0,0 +1,66 @@
+import AbstractObject from './abstract_object'
+
+  /**
+   * Abstract Crud Object
+   * Facebook Object basic persistence functions
+   * @extends AbstractObject
+   */
+export default class AbstractCrudObject extends AbstractObject {
+
+  /**
+   * @param {array} fields
+   * @param {object} data Initial data
+   */
+  constructor (fields, data = {}) {
+    super(fields, data)
+    this._changes = {}
+  }
+
+  /**
+   * Define data getter and setter for every field recording changes
+   */
+  _defineProperties () {
+    this._fields.forEach((field) => {
+      Object.defineProperty(this, field, {
+        get: () => this._data[field],
+        set: (value) => {
+          this._changes[field] = value
+          this._data[field] = value
+        },
+        enumerable: true
+      })
+    })
+  }
+
+  /**
+   * Set object data as if it were read from the server. Wipes related changes
+   * @param {object} data
+   * @return this
+   */
+  setData (data) {
+    super.setData(data)
+    if (this._changes) {
+      Object.keys(data).forEach((key) => {
+        delete this._changes[key]
+      })
+    }
+    return this
+  }
+
+  /**
+   * Export changed object data
+   * @return {object}
+   */
+  exportData () {
+    return this._changes
+  }
+
+  /**
+   * Clear change history
+   * @return this
+   */
+  clearHistory () {
+    this._changes = {}
+    return this
+  }
+}
---
+++ test/objects/core/test_abstract_crud_object.js
@@ -0,0 +1,43 @@
+import AbstractCrudObject from './../../../src/objects/core/abstract_crud_object'
+import { should } from 'chai'
+should()
+
+describe('AbstractCrudObject', () => {
+  class ConcreteCrudObject extends AbstractCrudObject {
+    constructor (data = {}) {
+      super(ConcreteCrudObject.fields, data)
+    }
+    static get fields () {
+      return Object.freeze({
+        field: 'field',
+        field2: 'field2'
+      })
+    }
+  }
+
+  it('should store changes for field properties', () => {
+    const object = new ConcreteCrudObject()
+    object.field = 3
+    object._changes.field.should.be.equal(3)
+  })
+
+  it('should set data wiping change history', () => {
+    const object = new ConcreteCrudObject()
+    object.field = 3
+    object.setData({'field': 3})
+    object.exportData().should.be.eql({})
+  })
+
+  it('should export changed data', () => {
+    const object = new ConcreteCrudObject()
+    object.field = 3
+    object.exportData().should.be.eql({'field': 3})
+  })
+
+  it('should clear change history', () => {
+    const object = new ConcreteCrudObject()
+    object.field = 3
+    object.clearHistory()
+    object.exportData().should.be.eql({})
+  })
+})

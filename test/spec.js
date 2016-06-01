318f65214ed5286b5358203cd64eedd0ce52ae22
Lucas Costa <lucas@lucascosta.com>
2016-06-01 11:26:06 -0300

    removing sinon

--- test/spec.js
+++ test/spec.js
@@ -2,9 +2,7 @@ if (typeof define === 'function' && define.amd) {
   requirejs.config({
     paths: {
       'mocha': './../bower_components/mocha/mocha',
-      'chai': './../bower_components/chai/chai',
-      'sinon': './../bower_components/sinonjs/sinon',
-      'sinon-chai': './../bower_components/sinon-chai/lib/sinon-chai'
+      'chai': './../bower_components/chai/chai'
     },
     shim: {
       'mocha': {
@@ -12,20 +10,13 @@ if (typeof define === 'function' && define.amd) {
           this.mocha.setup('bdd')
           return this.mocha
         }
-      },
-      'sinon': {
-        init: function () {
-          return this.sinon
-        }
       }
     }
   })
 
   require([
     'mocha',
-    'chai',
-    'sinon',
-    'sinon-chai'
+    'chai'
   ], function () {
     require([
       './suite'

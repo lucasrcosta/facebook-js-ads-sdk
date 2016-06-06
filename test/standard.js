4e55aad79af522a1a42e592bf02c1fe5dd99900f
Lucas Costa <lucas@lucascosta.com>
2016-06-06 03:44:22 -0300

    extending standards timeout

--- test/standard.js
+++ test/standard.js
@@ -1,4 +1,5 @@
-describe('Standard JS', () => {
+describe('Standard JS', function () {
+  this.timeout(5000)
   var standard = require('mocha-standard')
   it('should be respected', standard.files(['src/**/*.js', 'test/**/*.js']))
 })

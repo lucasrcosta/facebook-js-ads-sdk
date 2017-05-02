59b6163709bb15900bf438d750501bd31387c363
Talha Asad <talha@fallentech.com>
2017-05-02 11:58:12 +0500

    added basic ad pixel support

--- src/objects/ad-account.js
+++ src/objects/ad-account.js
@@ -7,6 +7,7 @@ import Campaign from './campaign'
 import Insights from './insights'
 import ReachFrequencyPredictions from './reach-frequency-prediction'
 import User from './user'
+import AdPixel from './ad-pixel'
 import CustomAudience from './custom-audience'
 
 /**
@@ -123,6 +124,10 @@ export default class AdAccount extends AbstractCrudObject {
 
   getUsers (fields, params, fetchFirstPage) {
     return this.getEdge(User, fields, params, fetchFirstPage)
+  }
+
+  getPixels (fields, params, fetchFirstPage) {
+    return this.getEdge(AdPixel, fields, params, fetchFirstPage)
   }
 
   getCustomAudiences (fields, params, fetchFirstPage) {
